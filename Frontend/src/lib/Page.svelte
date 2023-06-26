c69386a25972ed7496f6aa1457eb115395213676
LKSprod <lks.productions.lks@gmail.com>
2023-06-26 21:11:50 +0200

    luca update

--- Frontend/src/lib/Page.svelte
+++ Frontend/src/lib/Page.svelte
@@ -10,10 +10,12 @@
 
 <div class=" w-full h-full p-20">
     <div class="  w-full h-full">
-     <h1 class="font-MonoSpace  text-5xl font-regular bg-black pd-4 ml-5">Home</h1>
+     <h1 class="  text-5xl font-regular bg-black pd-4 ml-5">Home</h1>
      <div class="bg-drei h-2 mt-4 rounded-lg  "/>
+     <div class="w-4/5 pt-10 pl-10 pr-10 font-PublicSans">
+        <h1 class="bg-black">Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test </h1>
+        <button class="btn btn-secondary rounded-none">Test</button>
     </div>
-    <div class="">
-
     </div>
+    
 </div>
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,54 +3,71 @@ const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  daisyui: {
+    themes: [
+      {
+        base: {
+          "primary": "#bccdd6",
+          "secondary": "#93bfcf",
+          "accent": "#6096b4",
+          "neutral": "#15232a",
+          "base-100": "#eee9da",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        }
+      }
+    ]
+  },
   theme: {
-    fontFamily:{
-      monoSpace: ['SPACEMONO-BOLD.TTF','SPACEMONO-BOLDITALIC.TTF','SPACEMONO-ITALIC.TTF','SPACEMONO-REGULAR.TTF',],
-      publicSans:['public-sans'],
+    fontFamily: {
+      monoSpace: ['SPACEMONO-BOLD.TTF', 'SPACEMONO-BOLDITALIC.TTF', 'SPACEMONO-ITALIC.TTF', 'SPACEMONO-REGULAR.TTF',],
+      publicSans: ['public-sans'],
     },
     colors: {
-      background:' colors.white',
-      text: colors.black,
-      hover: '#00000030',
-      transparent: '#00000000',
-      
-      //Colourpallette
-      eins:   '#EEE9DA',
-      zwei:   '#BDCDD6',     
-      drei:   '#93BFCF',
-      vier:   '#6096B4',
-      fünf:   '#15232A',
-      
+      // background:' colors.white',
+      // text: colors.black,
+      // hover: '#00000030',
+      // transparent: '#00000000',
 
-      //Luca Farben
-      primary: {
-        DEFAULT: colors.blue[800],
-        hover: '#00000030',
-        text: colors.white,
-      },
-      secondary: {
-        DEFAULT: colors.blue[500],
-        hover: '#00000030',
-        text: colors.white,
-      },
-      accent: {
-        DEFAULT: colors.blue[700],
-        hover: colors.blue[600],
-        text: colors.gray[700],
-      },
-      success: {
-        DEFAULT: colors.green[500],
-        dark: colors.green[600],
-        darker: colors.green[700],
-      },
-      danger: {
-        DEFAULT: colors.red[600],
-        dark: colors.red[700],
-        darker: colors.red[800],
-      },
+      // //Colourpallette
+      // eins:   '#EEE9DA',
+      // zwei:   '#BDCDD6',     
+      // drei:   '#93BFCF',
+      // vier:   '#6096B4',
+      // fünf:   '#15232A',
+
+
+      // //Luca Farben
+      // primary: {
+      //   DEFAULT: colors.blue[800],
+      //   hover: '#00000030',
+      //   text: colors.white,
+      // },
+      // secondary: {
+      //   DEFAULT: colors.blue[500],
+      //   hover: '#00000030',
+      //   text: colors.white,
+      // },
+      // accent: {
+      //   DEFAULT: colors.blue[700],
+      //   hover: colors.blue[600],
+      //   text: colors.gray[700],
+      // },
+      // success: {
+      //   DEFAULT: colors.green[500],
+      //   dark: colors.green[600],
+      //   darker: colors.green[700],
+      // },
+      // danger: {
+      //   DEFAULT: colors.red[600],
+      //   dark: colors.red[700],
+      //   darker: colors.red[800],
+      // },
     },
   },
 
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
